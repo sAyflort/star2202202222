63d141c9fe45f28f5e90b4c3f7546742b21f3403
sAyflort <neefe.saratan.life.life@gmail.com>
2022-03-02 13:49:42 +0300

    Initial commit

---
+++ core/src/com/star/app/game/GameController.java
@@ -0,0 +1,65 @@
+package com.star.app.game;
+
+import com.badlogic.gdx.math.MathUtils;
+import com.star.app.screen.ScreenManager;
+
+public class GameController {
+    private Background background;
+    private BulletController bulletController;
+    private AsteroidController asteroidController;
+    private Hero hero;
+
+    public AsteroidController getAsteroidController() {
+        return asteroidController;
+    }
+
+    public BulletController getBulletController() {
+        return bulletController;
+    }
+
+    public Background getBackground() {
+        return background;
+    }
+
+    public Hero getHero() {
+        return hero;
+    }
+
+    public GameController() {
+        this.background = new Background(this);
+        this.bulletController = new BulletController();
+        this.asteroidController = new AsteroidController(this);
+        this.hero = new Hero(this);
+
+        for (int i = 0; i < 3; i++) {
+            asteroidController.setup(MathUtils.random(0, ScreenManager.SCREEN_WIDTH),
+                    MathUtils.random(0, ScreenManager.SCREEN_HEIGHT),
+                    MathUtils.random(-150, 150), MathUtils.random(-150, 150), 1.0f);
+        }
+    }
+
+    public void update(float dt) {
+        background.update(dt);
+        bulletController.update(dt);
+        asteroidController.update(dt);
+        hero.update(dt);
+        checkCollisions();
+    }
+
+
+    public void checkCollisions() {
+        for (int i = 0; i < bulletController.getActiveList().size(); i++) {
+            Bullet b = bulletController.getActiveList().get(i);
+            for (int j = 0; j < asteroidController.getActiveList().size(); j++) {
+                Asteroid a = asteroidController.getActiveList().get(j);
+                if (a.getHitArea().contains(b.getPosition())) {
+                    b.deactivate();
+                    if (a.takeDamage(1)) {
+                        hero.addScore(a.getHpMax() * 100 );
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
